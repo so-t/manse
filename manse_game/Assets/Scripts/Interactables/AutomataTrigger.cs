c845c0004519e6b4434274a5347312a1328fd1b2
so-t <sophthurmond@gmail.com>
2023-08-08 11:07:02 -0700

    Added basic AutomataTrigger class extending Interactable.cs

---
+++ manse_game/Assets/Scripts/Interactables/AutomataTrigger.cs
@@ -0,0 +1,16 @@
+﻿using Automata;
+using UnityEngine;
+
+namespace Interactables
+{
+    public class AutomataTrigger : Interactable
+    {
+        [SerializeField] private BaseAutomata automata;
+
+        protected override void Action() => automata.Run();
+
+        protected override bool ExitCondition() => automata.HasFinished();
+
+        protected override void ActionExit() => automata.Exit();
+    }
+}
