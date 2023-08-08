61091b780b0945d0647ad4d82ad26b56139939b4
so-t <sophthurmond@gmail.com>
2023-08-08 11:52:49 -0700

    AutomataTrigger now works on a collection of Automatons, rather than a single one

--- manse_game/Assets/Scripts/Interactables/AutomataTrigger.cs
+++ manse_game/Assets/Scripts/Interactables/AutomataTrigger.cs
@@ -1,16 +1,26 @@
-﻿using Automata;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Automata;
+using UnityEngine.Serialization;
 
 namespace Interactables
 {
     public class AutomataTrigger : BaseInteractable
     {
-        [SerializeField] private BaseAutomata automata;
+        [FormerlySerializedAs("Automata")] public List<BaseAutomata> automata;
 
-        protected override void Action() => automata.Run();
+        protected override void Action()
+        {
+            foreach (var automaton in automata)
+                automaton.Run();
+        }
 
-        protected override bool ExitCondition() => automata.HasFinished();
+        protected override bool ExitCondition() => automata.All(automaton => automaton.HasFinished());
 
-        protected override void Exit() => automata.Exit();
+        protected override void Exit()
+        {
+            foreach (var automaton in automata)
+                automaton.Exit();
+        }
     }
 }
