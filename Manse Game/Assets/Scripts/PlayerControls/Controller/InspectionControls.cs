bc232df3d2e720d44fdfc30763fd0c72fdef5a05
so-t <sophthurmond@gmail.com>
2023-10-11 17:34:06 -0700

    Rotation now happens in relation to world space rather than local. Added mouse controls.

--- Manse Game/Assets/Scripts/PlayerControls/Controller/InspectionControls.cs
+++ Manse Game/Assets/Scripts/PlayerControls/Controller/InspectionControls.cs
@@ -4,13 +4,7 @@ namespace PlayerControls.Controller
 {
     public class InspectionControls : MonoBehaviour
     {
-        public int Speed = 200;
-        private Rigidbody _rb;
-
-        private void Awake()
-        {
-            _rb = gameObject.GetComponent<Rigidbody>();
-        }
+        private const float Speed = 30;
 
         public void Destroy() => Destroy(this);
 
@@ -18,11 +12,26 @@ namespace PlayerControls.Controller
         {
             if (Input.GetAxisRaw("Horizontal") != 0.0f || Input.GetAxisRaw("Vertical") != 0.0f)
             {
-                var v = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-                var delta = Quaternion.Euler(v * (Speed * Time.fixedDeltaTime));
-                _rb.MoveRotation(_rb.rotation * delta);
+             
+                var worldRotation = new Vector3(
+                    Input.GetAxisRaw("Vertical"), 
+                    Input.GetAxisRaw("Horizontal"), 
+                    0);
+                var localRotation = transform.InverseTransformVector(worldRotation);
+                
+                transform.Rotate(localRotation * (Speed * Time.fixedDeltaTime));
             }
-            if (Input.GetKeyDown("escape"))
+            else if (Input.GetMouseButton(0))
+            {
+                var worldRotation = new Vector3(
+                    Input.GetAxis("Mouse Y"), 
+                    -Input.GetAxis("Mouse X"), 
+                    0);
+                var localRotation = transform.InverseTransformVector(worldRotation);
+                
+                transform.Rotate(localRotation * (2 * Speed * Time.fixedDeltaTime));
+            }
+            else if (Input.GetKeyDown("escape"))
             {
                 Destroy(this);
             }
